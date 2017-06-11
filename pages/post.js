d2a3431c8e7fdbed0e9f547e19d3283468ca4e3f
jo12bar <johann.b@telus.net>
2017-06-11 14:54:56 -0700

    Style markdown in posts

--- pages/post.js
+++ pages/post.js
@@ -1,10 +1,45 @@
-import Layout from '../components/Layout.js';
+import dedent from 'dedent';
 import fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout.js';
+import Markdown from 'react-markdown';
+import { fonts } from '../theme';
 
 const Post = ({ url }) => (
   <Layout>
     <h1>{url.query.title}</h1>
-    <p>This is the blog post content.</p>
+
+    <div className='markdown'>
+      <Markdown source={dedent`
+        This is our blog post.
+        Yes. We can have a [link](/link).
+        And we can have a title as well.
+
+        ### This is a title.
+
+        And here's the content.
+      `} />
+    </div>
+
+    <style jsx global>{`
+      .markdown {
+        font-family: ${fonts.default};
+      }
+
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      .markdown a:hover {
+        opacity: .6;
+      }
+
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
 );
 
